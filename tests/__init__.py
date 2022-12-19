834aa66b67dd72fa99508cd523d38aa1e814e653
Dan Roberts <danroberts728@gmail.com>
2022-12-18 20:38:12 -0600

    Try different path string

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../MilestoneTrigger')))
